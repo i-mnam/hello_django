5603a4d93e623a86fce24c116b77527f033856de
i-mnam <8linkbv@gmail.com>
2019-01-01 20:03:12 +0900

    dojo.views.mysum에서 다수 숫자를 받도록 개선

--- dojo/urls.py
+++ dojo/urls.py
@@ -4,9 +4,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [ # 패턴과 주소가 꼭 1:1대응일 필요 없다!!
-    url(r'^sum/(?P<x>\d+)/$', views.mysum), 
-    # 주의 여기서 "$"가 없었으면 ~/sum/01/23/4134 가 들어오더라도 
-    # # 모두 첫번쨰 패턴으로 잡힌다. 끝니없으니까!
-    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
-    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
+    url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum), 
+    # 끝에 왜 /$ 인지 "/"이게 없으면 왜 안되는지 모르겠다.
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -4,9 +4,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def mysum(request, x, y=0, z=0):
-    # request : HttpRequest
-    return HttpResponse("x:"+ str(int(x)+1) 
-        + "y: " + str(int(y)+10)
-        + "z: " + str(int(z) + 100)
-        ) 
+def mysum(request, numbers):
+    # numbers = "1/4526/231/3/252/457"
+    # return HttpResponse(numbers)
+    result = sum(map(lambda s: int(s or 0), numbers.split("/")))
+    # s가 거짓일 때 (or의 뜻) 0으로 치환
+    print("result:", result)
+    return HttpResponse(result)
