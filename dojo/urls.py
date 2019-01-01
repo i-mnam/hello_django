019938c7e1ec4fc4870d18439869ac46525313f4
i-mnam <8linkbv@gmail.com>
2019-01-01 19:41:07 +0900

    [3th] dojo.views.mysum에서 x/y/z/인자 3개를 처리

--- dojo/urls.py
+++ dojo/urls.py
@@ -1,7 +1,12 @@
 # dojo/urls.py
 
 from django.conf.urls import url
+from . import views
 
-urlpatterns = [
-    
+urlpatterns = [ # 패턴과 주소가 꼭 1:1대응일 필요 없다!!
+    url(r'^sum/(?P<x>\d+)/$', views.mysum), 
+    # 주의 여기서 "$"가 없었으면 ~/sum/01/23/4134 가 들어오더라도 
+    # # 모두 첫번쨰 패턴으로 잡힌다. 끝니없으니까!
+    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
+    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -1,3 +1,12 @@
+# dojo.views.py
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def mysum(request, x, y=0, z=0):
+    # request : HttpRequest
+    return HttpResponse("x:"+ str(int(x)+1) 
+        + "y: " + str(int(y)+10)
+        + "z: " + str(int(z) + 100)
+        ) 
