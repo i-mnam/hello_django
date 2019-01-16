e7af3544297b49ac4d8b45bbed37f90298d8aa97
i-mnam <8linkbv@gmail.com>
2019-01-16 21:37:42 +0900

    settings.AUTH_USER_MODEL올바른 지정 및 Comments의 __str__ 오류 수정

--- blog/models.py
+++ blog/models.py
@@ -44,8 +44,8 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.post # print by title
+    #def __str__(self):
+    #    return self.message # print by title
 
 
 class Tag(models.Model):
