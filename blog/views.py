d2cc0b447a923d78ee22dd26416c56a69f136c29
i-mnam <8linkbv@gmail.com>
2019-01-19 21:07:54 +0900

    blog.views.post_new/post_edit 뷰를 구현

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 # blog/views.py
 
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -44,3 +45,35 @@ def post_detail(request, id):
     return render(request, 'blog/post_detail.html', {
         'post': post,
     })
+
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post) 
+            # get_absolute_url() 설정을 model에 해놔서 => post_detail.html
+
+    else:
+        form = PostForm()
+        return render(request, 'blog/post_form.html', {
+            'form' : form,
+        })
+
+
+def post_edit(request, id):
+    post = get_object_or_404(Post, id=id)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post) 
+            # get_absolute_url() 설정을 model에 해놔서 => post_detail.html
+
+    else:
+        form = PostForm(instance=post)
+        return render(request, 'blog/post_form.html', {
+            'form' : form,
+        })
