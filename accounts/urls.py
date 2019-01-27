80906b1cf92c0951cfb3f01a72d4e26046b3ed5d
i-mnam <8linkbv@gmail.com>
2019-01-27 19:16:23 +0900

    로그인/로그아웃 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,20 @@
 # account/urls.py
 
+from django.conf import settings
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^profile/$', views.profile),
+    url(r'^login/$', auth_views.login, name='login'
+        , kwargs={'template_name': 'accounts/login_form.html'}),
+    url(r'^logout/$', auth_views.logout, name='logout'
+       , kwargs={'next_page': settings.LOGIN_URL }), 
+       # default '/accounts/logout' << admin mode..
+       # kwargs의 next_page지정으로 인해 LOGIN_URL = '/accounts/login/' 로 리다이렉트
+    url(r'^profile/$', views.profile, name='profile'), 
+    # ep32 p4  관련 default settings 
+    # LOGIN_REDIRECT_URL = '/accounts/profile/'
 ]
