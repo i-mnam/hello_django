4331064b3abd3558cd4c4e5742f4d05c6a4d2e34
i-mnam <8linkbv@gmail.com>
2019-01-27 16:18:20 +0900

    CBV : DeleteView 구현 - reverse_lazy 적용

--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,6 +1,7 @@
 # blog/view_cbv.py
 
 from django import forms
+from django.core.urlresolvers import reverse, reverse_lazy
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from .models import Post
 
@@ -25,4 +26,8 @@ class PostCreateView(CreateView):
 post_new = CreateView.as_view(model=Post)
 
 post_edit = UpdateView.as_view(model=Post, fields='__all__')
-post_delete = DeleteView.as_view(model=Post, success_url='/blog/')
+#post_delete = DeleteView.as_view(model=Post, success_url='/blog/')
+post_delete = DeleteView.as_view(model=Post, success_url=reverse_lazy('blog:post_list'))# reverse
+'''  현재 프로젝트가 초기화 되는 시점에 reverse를 수행하려 해서 error나는 것. 초기화 후에 실행되어야 하므로 > reverse_lazy
+django.core.exceptions.ImproperlyConfigured: The included URLconf 'askdjango.urls' does not appear to have any patterns in it. If you see valid patterns in the file then the issue is probably caused by a circular import.
+'''
