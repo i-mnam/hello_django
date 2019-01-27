27fde9d339c2d5e2ac38b1668c9bcc1b0b750344
i-mnam <8linkbv@gmail.com>
2019-01-27 14:57:50 +0900

    CBV : DeatilView적용

--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,11 +1,12 @@
 # blog/view_cbv.py
 
 from django import forms
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView
 from .models import Post
 
 post_list = ListView.as_view(model=Post, paginate_by=10)
 # http://localhost:8080/blog/?page=2
+post_detail = DetailView.as_view(model=Post, pk_url_kwarg='id')
 
 # blog/forms.py
 class PostForm(forms.ModelForm):
